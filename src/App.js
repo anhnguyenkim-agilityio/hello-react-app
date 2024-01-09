ddeb1bab8d402045aa517bed13e419ce04c7c318
Anh Nguyen <anh.nguyenkim@asnet.com.vn>
2024-01-09 17:14:56 +0700

    Add the Deploy to Amplify Hosting button

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <h1>Anh Nguyen</h1>
         <img src={logo} className="App-logo" alt="logo" />
+        <a href="https://console.aws.amazon.com/amplify/home#/deploy?repo=https://github.com/anhnguyenkim-agilityio/hello-react-app">
+          <img src="https://oneclick.amplifyapp.com/button.svg" alt="Deploy to Amplify Hosting"/>
+        </a>
       </header>
     </div>
   );
