0e1c5a0e12e14e7c52f90b9a5d768319ad018f8c
Anh Nguyen <anh.nguyenkim@asnet.com.vn>
2024-01-09 17:27:40 +0700

    Test preview PR

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Anh Nguyen</h1>
+        <p>Test pull request previews</p>
         <img src={logo} className="App-logo" alt="logo" />
         <a href="https://console.aws.amazon.com/amplify/home#/deploy?repo=https://github.com/anhnguyenkim-agilityio/hello-react-app">
           <img src="https://oneclick.amplifyapp.com/button.svg" alt="Deploy to Amplify Hosting"/>
