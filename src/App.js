904bd2264b8c8307f181ff66a27c4238caea1638
Anh Nguyen <anh.nguyenkim@asnet.com.vn>
2024-01-11 15:20:06 +0700

    Update test

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Anh Nguyen</h1>
+        <p>Test pull request previews</p>
+        <small>You are running this application in <b>{process.env.REACT_APP_ENVIRONMENT}</b> environment and API endpoint is <b>{process.env.REACT_APP_API_ENDPOINT}</b>.</small>
         <img src={logo} className="App-logo" alt="logo" />
         <a href="https://console.aws.amazon.com/amplify/home#/deploy?repo=https://github.com/anhnguyenkim-agilityio/hello-react-app">
           <img src="https://oneclick.amplifyapp.com/button.svg" alt="Deploy to Amplify Hosting"/>
+          Deploy to Amplify Hosting
         </a>
       </header>
     </div>
